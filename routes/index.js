3bf0a59eb45ca70ee829a1eb0babed9d62bc89b1
Avi Bagla <avibagla@gmail.com>
2014-04-25 22:25:35 -0700

    basic implementation of the main page with data

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
+var testData = require('../data/stories.json');
+
+
 function randSort(){
 	return(Math.round(Math.random()) - 0.5);
 }
 
 exports.view = function(req, res){
-	res.render('index');
+	res.render('index', testData);
 }
+
+
+
+/*{ 
+			"title": "",
+			"details": [
+				{"text": ""},
+				{"text": ""},
+				{"text": ""}
+			],
+			"questions":[
+				{"text": ""},
+				{"text": ""},
+				{"text": ""}
+			]
+		},
+
+*/
