7b50cdf040cb19bec54fe92589f7b42937152d1e
Avi Bagla <avibagla@gmail.com>
2014-05-08 15:05:36 -0700

    working on newsItem

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('pink unicorns'));
+app.use(express.cookieParser('pink unicorns are beautiful'));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
