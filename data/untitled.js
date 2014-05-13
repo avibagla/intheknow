45c7bcbb27188730812a782307f9de15fc13ea24
Avi Bagla <avibagla@gmail.com>
2014-05-12 20:21:31 -0700

    added slider

---
+++ data/untitled.js
@@ -0,0 +1,16 @@
+
+array = ["james", "jack", "john", "adam", "blues", "jill"];
+
+function main(array){
+
+	//sort the array
+	array.sort();
+
+	//put jack and jill towards the front
+	array.unshift("jill");
+	array.unshift("jack");
+	
+
+
+
+}
