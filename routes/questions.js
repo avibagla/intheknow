26a7fea213ff5d260922e75abe73df49c88eaa2c
Shailesh Tainwala <shailesh.tainwala@gmail.com>
2014-05-20 00:55:26 -0700

    Modified JSON file and submit questions to store number of votes

--- routes/questions.js
+++ routes/questions.js
@@ -16,6 +16,12 @@ exports.createQuestion = function(req, res){
 	storyID = findIndexByAttr(stories.stories, 'uniqueID', newsId);
 	story = stories.stories[storyID];
 	console.log(story);
+	//temp
+	var a = story['questions'][0]['yes'] + story['questions'][0]['no'] + 3
+	console.log("YES:"+story['questions'][0]['yes']);
+	console.log("NO:"+story['questions'][0]['no']);
+	console.log("a="+a);
+	//endTemp
 	res.render('createQuestions', story);
 }
 
@@ -29,8 +35,8 @@ exports.submitQuestion = function(req, res){
 	
 	storyID = findIndexByAttr(stories.stories, 'uniqueID', newsId);
 	story = stories.stories[storyID];
-	story['questions'].push({"text": qs});
-	fs.writeFileSync('data/stories.json', JSON.stringify(stories));
+	story['questions'].push({"text": qs, "yes": 0, "no": 0});
+	fs.writeFileSync('data/stories.json', JSON.stringify(stories, null, 4));
 
 	var news = require('./news')
 	news.viewNewsItem(req, res);
