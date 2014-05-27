a6df563ea43face4f936e159d4ef58b6e2786229
Avi Bagla <avibagla@gmail.com>
2014-05-27 01:53:24 -0700

    fixed bug on continual voting

--- public/js/newsItem.js
+++ public/js/newsItem.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	initializePage();
 });
 
+var beenClicked = false;
 
 function initializePage(){
 
@@ -12,63 +13,69 @@ function initializePage(){
 		var uniqueID = $('#uniqueID').attr("v");
 		//http://api.jquery.com/attribute-equals-selector/
 		//console.log("OuterThis"+this.innerHTML);
-		$.ajax({
-			url: "/updateVotes",
-			data: {"uniqueID": uniqueID,"index": qsNumber, "vote": "yes"},
-			type: "GET",
-			dataType: "json",
-			success: function(json) {
-				console.log("SUCCESS");
-				var yesVotes = json['yes'];
-				var noVotes = json['no'];
-				console.log("Yes:"+yesVotes+"   No:"+noVotes);
-				var yesRelativeWidth = 98*yesVotes/(yesVotes+noVotes);
-				var noRelativeWidth = 98*noVotes/(yesVotes+noVotes);
-				console.log("YesWidth:"+yesRelativeWidth+"   NoWidth:"+noRelativeWidth);
-			//	console.log("This:"+this);
-				$(".yes[qsNumber='"+qsNumber+"']").width((yesRelativeWidth-1)+"%");
-				$(".yes[qsNumber='"+qsNumber+"']").removeClass('yes').addClass('yesClicked');
-				$(".no[qsNumber='"+qsNumber+"']").width((noRelativeWidth-1)+"%");
-				$(".no[qsNumber='"+qsNumber+"']").removeClass('no').addClass('noClicked');
-			},
-			error: function(xhr, status, errorThrown) {
-				console.log("ERROR!");
-			},
-			complete: function(xhr, status) {
-				console.log("COMPLETE!");
-			}
-		});
+		if(!beenClicked){	
+			$.ajax({
+				url: "/updateVotes",
+				data: {"uniqueID": uniqueID,"index": qsNumber, "vote": "yes"},
+				type: "GET",
+				dataType: "json",
+				success: function(json) {
+					console.log("SUCCESS");
+					var yesVotes = json['yes'];
+					var noVotes = json['no'];
+					console.log("Yes:"+yesVotes+"   No:"+noVotes);
+					var yesRelativeWidth = 98*yesVotes/(yesVotes+noVotes);
+					var noRelativeWidth = 98*noVotes/(yesVotes+noVotes);
+					console.log("YesWidth:"+yesRelativeWidth+"   NoWidth:"+noRelativeWidth);
+				//	console.log("This:"+this);
+					$(".yes[qsNumber='"+qsNumber+"']").width((yesRelativeWidth-1)+"%");
+					$(".yes[qsNumber='"+qsNumber+"']").removeClass('yes').addClass('yesClicked');
+					$(".no[qsNumber='"+qsNumber+"']").width((noRelativeWidth-1)+"%");
+					$(".no[qsNumber='"+qsNumber+"']").removeClass('no').addClass('noClicked');
+				},
+				error: function(xhr, status, errorThrown) {
+					console.log("ERROR!");
+				},
+				complete: function(xhr, status) {
+					console.log("COMPLETE!");
+					beenClicked = true;
+				}
+			});
+		}
 	});
 
 	$('.no').click(function(){
 		var qsNumber = this.getAttribute("qsNumber");
 		var uniqueID = $('#uniqueID').attr("v")
 		//http://api.jquery.com/attribute-equals-selector/
-		$.ajax({
-			url: "/updateVotes",
-			data: {"uniqueID": uniqueID,"index": qsNumber, "vote": "no"},
-			type: "GET",
-			dataType: "json",
-			success: function(json) {
-				console.log("SUCCESS");
-				var yesVotes = json['yes'];
-				var noVotes = json['no'];
-				console.log("Yes:"+yesVotes+"   No:"+noVotes);
-				var yesRelativeWidth = 98*yesVotes/(yesVotes+noVotes);
-				var noRelativeWidth = 98*noVotes/(yesVotes+noVotes);
-				console.log("YesWidth:"+yesRelativeWidth+"   NoWidth:"+noRelativeWidth);
-			//	console.log("This:"+this);
-				$(".yes[qsNumber='"+qsNumber+"']").width((yesRelativeWidth-1)+"%");
-				$(".yes[qsNumber='"+qsNumber+"']").removeClass('yes').addClass('yesClicked');
-				$(".no[qsNumber='"+qsNumber+"']").width((noRelativeWidth-1)+"%");
-				$(".no[qsNumber='"+qsNumber+"']").removeClass('no').addClass('noClicked');
-			},
-			error: function(xhr, status, errorThrown) {
-				console.log("ERROR!");
-			},
-			complete: function(xhr, status) {
-				console.log("COMPLETE!");
-			}
-		});
+		if(!beenClicked){	
+			$.ajax({
+				url: "/updateVotes",
+				data: {"uniqueID": uniqueID,"index": qsNumber, "vote": "no"},
+				type: "GET",
+				dataType: "json",
+				success: function(json) {
+					console.log("SUCCESS");
+					var yesVotes = json['yes'];
+					var noVotes = json['no'];
+					console.log("Yes:"+yesVotes+"   No:"+noVotes);
+					var yesRelativeWidth = 98*yesVotes/(yesVotes+noVotes);
+					var noRelativeWidth = 98*noVotes/(yesVotes+noVotes);
+					console.log("YesWidth:"+yesRelativeWidth+"   NoWidth:"+noRelativeWidth);
+				//	console.log("This:"+this);
+					$(".yes[qsNumber='"+qsNumber+"']").width((yesRelativeWidth-1)+"%");
+					$(".yes[qsNumber='"+qsNumber+"']").removeClass('yes').addClass('yesClicked');
+					$(".no[qsNumber='"+qsNumber+"']").width((noRelativeWidth-1)+"%");
+					$(".no[qsNumber='"+qsNumber+"']").removeClass('no').addClass('noClicked');
+				},
+				error: function(xhr, status, errorThrown) {
+					console.log("ERROR!");
+				},
+				complete: function(xhr, status) {
+					console.log("COMPLETE!");
+					beenClicked = true;
+				}
+			});
+		}
 	});
 }
